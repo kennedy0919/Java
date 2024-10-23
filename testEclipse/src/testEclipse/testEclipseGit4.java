c21ee5374fb63f8f4d7474c2ce718e9ef82b031c
kennedy1105 <kennedy1234@naver.com>
2024-10-23 21:43:09 +0900

    20241023 java if_control

--- testEclipse/src/testEclipse/testEclipseGit4.java
+++ testEclipse/src/testEclipse/testEclipseGit4.java
@@ -3,8 +3,9 @@ package testEclipse;
 public class testEclipseGit4 {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+		// test git push
+		System.out.println("Hello World");
+		// ¹¹¾ß ¿Ö ¾È¹Ù²î´Â°Å¾ß
 	}
 
 }
